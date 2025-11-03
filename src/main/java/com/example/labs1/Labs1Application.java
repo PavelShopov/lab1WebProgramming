9fd4a482900bbe4d40bebca952f66ca91c6c0ea8
PavelShopov <shopovpavel223@gmail.com>
2025-11-03 16:48:14 +0100

    first ommit

---
+++ src/main/java/com/example/labs1/Labs1Application.java
@@ -0,0 +1,13 @@
+package com.example.labs1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Labs1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Labs1Application.class, args);
+    }
+
+}
